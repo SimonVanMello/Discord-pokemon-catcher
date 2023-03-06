714ebed8e1c0eafa2ec6383fe927b583fe78a28c
TropicoDebug <simon.vanmello@std.heh.be>
2023-03-06 16:34:09 +0100

    added giveme command

--- main.py
+++ main.py
@@ -1,17 +1,47 @@
-import discord, json, asyncio
+import discord, json, asyncio, re
 
 class Client(discord.Client):
-    async def on_connect(self):
+    async def on_connect(self: object):
         pokecount: int = 0
         try:
-            channel = client.get_channel(data["channel_id"])
             while True:
-                await channel.send(";pokemon")
+                await self.get_channel(data["channel_id"]).send(";pokemon")
                 pokecount += 1
                 print(f"{pokecount} pokemon caught")
-                await asyncio.sleep(60*60*3+5)
+                await asyncio.sleep(60*60*3+3)
         except Exception as e:
             print(f"error : {e}")
+
+    async def on_message(self: object, message: object):
+        async def sendPokemon(channel: object, message: object, pokemon: str):
+            try:
+                await channel.send(f";give {message.author.mention} {pokemon}")
+                await asyncio.sleep(1)
+                await channel.send("yes")
+                await asyncio.sleep(2)
+            except Exception as e:
+                print(f"error : {e}")
+
+        # only proceed the message if it starts with "giveme"
+        if not str(message.content).startswith("giveme"):
+            return
+        # check if the owner name is set in data.json
+        if not data["owner_name"]:
+            return
+        # check if the message author is the one specified in data.json
+        if str(message.author) != data["owner_name"]:
+            return
+        # check if the message was sent in the channel specified in data.json
+        if message.channel.id != data["channel_id"]:
+            return
+        
+        # tranform the message into a list of pokemons
+        pokemons = str(message.content)[6:].split(";")
+        
+        # give each pokemon of the list to the owner set in data.json
+        for pokemon in pokemons:
+            await sendPokemon(self.get_channel(data["channel_id"]), message, pokemon)
+
 
 def loadData() -> dict:
     with open("data.json") as f:
@@ -20,7 +50,11 @@ def loadData() -> dict:
 def validData(data: dict) -> bool:
     if type(data["channel_id"]) != int:
         return False
-    if not data["token"] or "." not in data["token"]:
+    if not data["bot_token"]:
+        return False
+    if len(data["bot_token"].split(".")) != 3:
+        return False
+    if data["owner_name"] and not re.search("^.{3,32}#[0-9]{4}$", data["owner_name"]):
         return False
     return True
 
@@ -28,6 +62,6 @@ def validData(data: dict) -> bool:
 data: dict = loadData()
 if validData(data):
     client = Client()
-    client.run(data["token"])
+    client.run(data["bot_token"])
 else:
     print("Invalid value in data.json")
